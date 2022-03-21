ee8d4b90d97a3729d2fabb9860bcf30fb9e4fa14
qa-alam <alam.itprofessional@gmail.com>
2022-03-20 21:55:24 -0400

    Updated

--- src/test/java/com/usa/stepdefinations/LoginStepDef.java
+++ src/test/java/com/usa/stepdefinations/LoginStepDef.java
@@ -1,5 +1,36 @@
 package com.usa.stepdefinations;
 
-public class LoginStepDef {
 
+
+public class LoginStepDef {
+/*	@Given("User able to open a any browser")
+	public void user_able_to_open_a_any_browser() {
+	    
+	    
+	}
+	@Given("User able to enter {string} url")
+	public void user_able_to_enter_url(String string) {
+	    
+	    
+	}
+	@When("User able to click on sign-in button")
+	public void user_able_to_click_on_sign_in_button() {
+	    
+	    
+	}
+	@When("User able to enter valid userName {string} & password {string}")
+	public void user_able_to_enter_valid_user_name_password(String string, String string2) {
+	    
+	    
+	}
+	@When("User able to click on login button")
+	public void user_able_to_click_on_login_button() {
+	    
+	    
+	}
+	@Then("User can verify the user information on the grid")
+	public void user_can_verify_the_user_information_on_the_grid() {
+	    
+	    
+	} */
 }
