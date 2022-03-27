c31d70feddb79be588da4933675d04c5aae022bc
qa-alam <alam.itprofessional@gmail.com>
2022-03-27 19:21:05 -0400

    Update collection class

--- src/test/java/com/usa/collections/Array_Object.java
+++ src/test/java/com/usa/collections/Array_Object.java
@@ -1,5 +1,74 @@
 package com.usa.collections;
 
+import java.util.Arrays;
+
 public class Array_Object {
+	
+	public static void main(String[] args) {
+		
+		String name1 = "";
+		String name2 = "";
+		String name3 = "";
+		String name4 = "";
+		String name5 = "";
+		String name6 = "";
+		String name7 = "";
+		
+		int a = 10;
+		     a = 20;
+		     a = 30;
+		     a = 40;
+		    // System.out.println(a);
+
+	//  What is an array 
+    // Array is an object as well as non-primitive data type 
+   // Array  can handle group of object in a single unit or variables 
+   // array i can use two way 
+  // One single dimension and multi-dimension
+   // What is dimension?
+  //  single dimension is [] &  multi-dimension [][]
+  // Why you need single dimension array 
+  // If i need to handle single column then i have to use single dimension [] esarray 
+   // If i need to handle multiple column & row then i have to use multi-dimension [][] array 	     
+		     
+    // What is drawback in array? dis-advantage 
+    //  Array length is fixed
+   // Array use the length keyword 
+	// declare a array 19th data is length     
+		     
+	//	  How to declare a array
+		int  num [] = new int [10];
+		num [0] = 10;
+		num [1] = 11;
+		num [2] = 12;
+		num [3] = 13;
+		num [4] = 14;
+		num [5] = 15;
+		num [6] = 16;
+		num [7] = 17;
+		num [8] = 18;
+		num [9] = 19;	
+		num [9] = 19;	
+		//num [10] = 20;   We are getting ArrayIndexOutOfBoundsException: 
+		// num [11] = 21;	
+		//System.out.println("Colling by index : "+ num[5]);
+		
+	int numbers[] = {10, 20, 30, 40, 60, 70, 80, 90, 100};	
+	
+	
+	String name[] = {"Tawhid","Alam","Rakib","Sumaya","Sumona","Emon","Shika","Taniya","Zara","Sharmin", 
+			                   "Jahan", "Bellal", "Sabiha","Mukta", "Farib", "Sagor", "Tanzir", "Shabuddin", "Zubair", "Refat"};
+	System.out.println("Colling by index : "+ name[5]);
+	
+	System.out.println(Arrays.asList(name));
+	
+	
+	int arr [][] =    {{1, 2, 3, 4, 5, 6},
+			                 { 7, 8, 9, 10, 11},  // Row  0, 1, 2  //  0=3  9=1
+			                 {12, 13, 14, 15}};
+	System.out.println(arr[2][2]);
+			           
+		
+	}
 
 }
--- src/test/java/com/usa/collections/List_Interface.java
+++ src/test/java/com/usa/collections/List_Interface.java
@@ -1,5 +1,77 @@
 package com.usa.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+import org.testng.annotations.Test;
+
 public class List_Interface {
 
+	// How to declare ArrayList, LinkedList, Vector & Stack
+	@Test
+	public static void getArrayList() {
+		// One way
+		// Non-Generic code, Java old version like 1.2 - 1.5
+		ArrayList object = new ArrayList();
+		object.add(1);
+		object.add("Alam");
+		object.add(14);
+
+		// 2nd way
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		// list.add("Alam");
+		list.add(1);
+
+		// Class to class create a object
+		ArrayList<String> name = new ArrayList<>();
+		name.add("Alam");
+		name.add("Tawhid");
+		name.add("Rakib");
+
+		// 3rd way
+		// Interface to class create instances
+		List<Integer> number = new ArrayList<>();
+		number.add(10);
+		number.add(20);
+		number.add(30);
+		number.add(49);
+
+		// 4th way
+		List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+		System.out.println(numbers);
+		// Basic loop
+		for (int i = 0; i < numbers.size(); i++) {
+			System.out.println(numbers.get(i));
+
+		}
+
+		// Advance loop
+		List<String> names = Arrays.asList("Selenium", "Java", "Maven", "Cucumber", "TestNG", "Apache POI");
+		for (String value : names) {
+			System.out.println(value);
+		}
+
+	}
+
+	public static void getLinkedList() {
+		LinkedList<String> link = new LinkedList<>();
+		link.add("Selenium");
+		link.add("Java");
+		link.add("Maven");
+		link.add("Cucumber");
+		link.add("TestNG");
+	}
+
+	public static void getVector() {
+		Vector<String> link = new Vector<>();
+		link.add("Selenium");
+		link.add("Java");
+		link.add("Maven");
+		link.add("Cucumber");
+		link.add("TestNG");
+	}
 }
